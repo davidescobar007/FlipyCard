c8070b3dee18a1ed077cca4e2c084e28b3a4de81
davidescobar007 <davidescobar007@github.com>
2022-09-03 23:48:38 -0500

    feature/ animations on cards

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
 import "../public/styles/animate.scss"
+import "../public/styles/customAnimations.scss"
 import "./main.scss"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
