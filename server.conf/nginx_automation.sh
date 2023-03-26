b4019956c00201814a7101c5cc84610f2d9ad4c6
davidescobar007 <davidescobar007@hotmail.com>
2023-03-25 21:14:25 -0500

    reverse proxy for pocketbase in docker container

---
+++ server.conf/nginx_automation.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Copy nginx.conf file to the nginx container
+docker cp default.conf server:/etc/nginx/conf.d/default.conf
+
+# Restart the nginx server
+docker exec server nginx -s reload
+
+# Output the contents of the new nginx.conf file inside the container
+docker exec server cat /etc/nginx/conf.d/default.conf
+
+# Display the error logs from nginx inside the container
+docker logs -f server 2>&1 | grep error
+
