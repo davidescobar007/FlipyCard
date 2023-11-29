71e44c6c5793d78ca5d6e2e70c3216cdecc58eb0
davidescobar007 <davidescobar007@hotmail.com>
2023-11-28 20:48:15 -0500

    bug fixing

--- src/components/molecules/protectedRoute/index.jsx
+++ src/components/molecules/protectedRoute/index.jsx
@@ -2,8 +2,6 @@ import { useTranslation } from "react-i18next"
 import { Navigate } from "react-router-dom"
 import PropTypes from "prop-types"
 
-import { handleErrorModal } from "../../../context/actions/global.actions"
-
 function ProtectedRoute({ children }) {
    const { t } = useTranslation()
    const user = JSON.parse(localStorage.getItem("pocketbase_auth"))
@@ -12,7 +10,7 @@ function ProtectedRoute({ children }) {
       return children
    } else {
       console.log("localstorage_noStorage", user)
-      handleErrorModal(t("constants.needSignUp.siu"))
+      // handleErrorModal(t("constants.needSignUp.siu"))
       return <Navigate to="/learn" />
    }
 }
